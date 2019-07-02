ad993f883b8f6f46f26ef345a3cbb987eba631c5
r07521706 <49805530+r07521706@users.noreply.github.com>
2019-07-02 14:56:14 +0800

    Update Sidebar.js
    add discription

--- chat/src/containers/Sidebar.js
+++ chat/src/containers/Sidebar.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux'
 import SidebarComponent from '../components/Sidebar'
 
+//这个函数的第一个参数就是 Redux 的 store
+//state => ({users: state.users})，
+//我们从中摘取了 users 属性。
+//因为返回了具有 users 属性的对象，所以 MyComp 会有名为 users 的 props 字段。
 export const Sidebar = connect(state => ({
 	users: state.users
 }), {})(SidebarComponent)
+
